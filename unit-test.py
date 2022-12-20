8fec937f7239392236b687134ff3855db9288060
Koen Arroo <koen.arroo@gmail.com>
2022-12-20 14:14:38 +0000

    Revert "Add bad test to check UT"
    This reverts commit f10276809a4e72333a7d5ca3b0b89bced47c1636.

--- unit-test.py
+++ unit-test.py
@@ -1,6 +1,6 @@
 import pytest
 from weather import get_image_path
 
-@pytest.mark.parametrize("input,expected",[("01d","./res/weather/sun_clr.png")])
+@pytest.mark.parametrize("input,expected",[("01d","./res/weather/sun_clear.png")])
 def test_get_image_path(input,expected):
     assert get_image_path(input) == expected
